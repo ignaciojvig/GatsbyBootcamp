9ad7db135bbfa9b544ff3e08dbdaba0abf3f75c6
João Victor Ignacio <joao.ignacio@venturus.org.br>
2021-05-20 09:28:23 -0300

    Improved Page Styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,23 +1,25 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import * as headerStyles from './header.module.scss'
+
 export const Header = () => {
   return (
-    <header>
-      <h1> João Victor Ignacio </h1>
+    <header className={headerStyles.header}>
+      <h1> <Link className={headerStyles.title} activeClassName={headerStyles.activeNavItem} to="/"> João Victor Ignacio </Link> </h1>
       <nav>
-        <ul>
+        <ul className={headerStyles.navList}>
           <li>
-            <Link to="/"> Home </Link>
+            <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/"> Home </Link>
           </li>
           <li>
-            <Link to="/blog"> Blog </Link>
+            <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/blog"> Blog </Link>
           </li>
           <li>
-            <Link to="/about"> About </Link>
+            <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/about"> About </Link>
           </li>
           <li>
-            <Link to="/contact"> Contact </Link>
+            <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/contact"> Contact </Link>
           </li>
         </ul>
       </nav>
--- src/components/layout.js
+++ src/components/layout.js
@@ -3,11 +3,15 @@ import { Header } from './header'
 import { Footer } from './footer'
 import "../styles/index.scss"
 
+import * as layoutStyles from './layout.module.scss'
+
 export const Layout = (props) => {
   return (
-    <div>
-      <Header />
-      {props.children}
+    <div className={layoutStyles.container}>
+      <div className={layoutStyles.content}>
+        <Header />
+        {props.children}
+      </div>
       <Footer />
     </div>
   )
